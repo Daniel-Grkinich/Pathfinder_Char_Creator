355d13180a169316503f5c6d25294c9aa953b46c
The-Data-is-a-lie <danielgrkinich@gmail.com>
2024-04-01 23:31:50 -0700

    production server

--- app.py
+++ app.py
@@ -79,4 +79,5 @@ if __name__ == '__main__':
     # Get port from environment variable PORT or default to 5000
     port = int(os.environ.get('PORT', 5000))
     # Run the app with host as 0.0.0.0 to allow connections from outside
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
+    # set debug=False for production servers
